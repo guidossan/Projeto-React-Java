15df5f3b579c870f28453a376b55b1f87fa4812a
Guilherme Martins <guimsl2004@gmail.com>
2024-06-13 15:14:14 -0300

    image controller

---
+++ demo/src/main/java/com/guilherme/demo/application/ImageController.java
@@ -0,0 +1,32 @@
+package com.guilherme.demo.application;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+
+@RestController
+@RequestMapping("/images")
+@Slf4j
+public class ImageController {
+
+
+
+    @SuppressWarnings("rawtypes")
+    @PostMapping
+    public ResponseEntity save(@RequestParam("file") MultipartFile file, @RequestParam("name")String name, @RequestParam("tags") List<String> tags){
+
+        log.info("Imagem recebida: name {}, size {}", file.getName(), file.getSize());
+        log.info("Nome definido para a imagem:  {}", name);
+        log.info("Tags: {}", tags);
+        return ResponseEntity.ok().build();
+    }
+}
