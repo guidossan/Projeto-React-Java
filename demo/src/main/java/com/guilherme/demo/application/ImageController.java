31569079c788a64c51f7341382c720ffed453436
Guilherme Martins <guimsl2004@gmail.com>
2024-06-18 16:51:38 -0300

    retorna codigo de created e aceita imagens maiores

--- demo/src/main/java/com/guilherme/demo/application/ImageController.java
+++ demo/src/main/java/com/guilherme/demo/application/ImageController.java
@@ -1,6 +1,7 @@
 package com.guilherme.demo.application;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.MediaType;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.guilherme.demo.domain.entity.Image;
 import com.guilherme.demo.domain.enums.ImageExtencion;
@@ -27,6 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ImageController {
 
     private final ImageService service;
+    private final ImagesMapper mapper;
 
     @PostMapping()
     public ResponseEntity<String> save(
@@ -39,14 +42,13 @@ public class ImageController {
       
        
        
-        Image image = Image.builder()
-                    .name(name)
-                    .tags(String.join(",", tags))
-                    .size(file.getSize())
-                    .extencion(ImageExtencion.valueOf(MediaType.valueOf(file.getContentType())))
-                    .file(file.getBytes())
-                    .build();
-        service.save(image);
-        return ResponseEntity.ok().build();
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage = service.save(image);
+        URI imageUri = buildImageUri(savedImage);
+        return ResponseEntity.created(imageUri).build();
+    }
+    private URI buildImageUri(Image image){
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder.fromCurrentRequest().path(imagePath).build().toUri();
     }
 }
---
+++ demo/src/main/java/com/guilherme/demo/application/ImagesMapper.java
@@ -0,0 +1,27 @@
+package com.guilherme.demo.application;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.guilherme.demo.domain.entity.Image;
+import com.guilherme.demo.domain.enums.ImageExtencion;
+
+@Component
+public class ImagesMapper {
+   
+
+    public Image mapToImage(MultipartFile file, String name, List<String> tags) throws IOException {
+        return Image.builder()
+                    .name(name)
+                    .tags(String.join(",", tags))
+                    .size(file.getSize())
+                    .extencion(ImageExtencion.valueOf(MediaType.valueOf(file.getContentType())))
+                    .file(file.getBytes())
+                    .build();
+        
+    }
+}
