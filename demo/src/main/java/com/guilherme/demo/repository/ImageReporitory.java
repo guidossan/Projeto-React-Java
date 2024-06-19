5c53febcd604b22d8bbbb5155bec277819e3cb2b
Guilherme Martins <guimsl2004@gmail.com>
2024-06-19 15:02:50 -0300

    query sql so que com specification em classe separada

--- demo/src/main/java/com/guilherme/demo/repository/ImageReporitory.java
+++ demo/src/main/java/com/guilherme/demo/repository/ImageReporitory.java
@@ -10,22 +10,23 @@ import org.springframework.util.StringUtils;
 import com.guilherme.demo.domain.entity.Image;
 import com.guilherme.demo.domain.enums.ImageExtencion;
 
+
 public interface ImageReporitory extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image>{
     default List<Image> findByExtencionAndNameOrTagsLike(ImageExtencion extencion, String query){
         //SELECT * FROM IMAGE WHERE 1 = 1
-        Specification<Image> conjuction = (root, q, criteriaBuilder) -> criteriaBuilder.conjunction();
-        Specification<Image> spec = Specification.where(conjuction);
+        
+        Specification<Image> spec = Specification.where(GenericSpec.conjunction());
 
         //AND EXTENCION ="PNG"
         if (extencion!= null){
-            Specification<Image> extencioEqual =  (root, q, cb) -> cb.equal(root.get("extencion"), extencion);
-            spec = spec.and(extencioEqual);
+
+            spec = spec.and(ImageSpec.extencionEqual(extencion));
         } 
         //AND(NAME LIKE 'QUERY' OR TAGS LIKE 'QUERY')
         //RIVER => %RI$
         if (StringUtils.hasText(query)){
-            Specification<Image> nameLike = (root, q, cb) -> cb.like(cb.upper(root.get("name")), "%"+query.toUpperCase()+"%");
-            Specification<Image> tagsLike = (root, q, cb) -> cb.like(cb.upper(root.get("tags")), "%"+query.toUpperCase()+"%");
+            Specification<Image> nameLike = ImageSpec.nameLike(query);
+            Specification<Image> tagsLike = ImageSpec.tagsLike(query);
             Specification<Image> nameOrTagsLike = Specification.anyOf(nameLike, tagsLike);
             spec = spec.and(nameOrTagsLike);
         }
