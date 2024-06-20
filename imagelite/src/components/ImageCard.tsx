8b63b3658ae8ad7ebbe69e46acc989e2d0d5974a
Guilherme Martins <guimsl2004@gmail.com>
2024-06-20 15:57:25 -0300

    download image

--- imagelite/src/components/ImageCard.tsx
+++ imagelite/src/components/ImageCard.tsx
@@ -1,3 +1,4 @@
+'use client'
 interface ImageCardProps {
     nome?: string;
     tamanho?: string;
@@ -6,8 +7,12 @@ interface ImageCardProps {
 }
 
 export const ImageCard: React.FC<ImageCardProps> = ({ nome, tamanho, dataUpload, src }: ImageCardProps) => {
+    function donwload(){
+        window.open(src, '_blank')
+    }
+    
     return(
-        <div className='card relative bg-white rounted-md shadow-md transition-transform ease-in duration-300 trasnform hover: shadow-lg hover:-translate-y-2'>
+        <div onClick={donwload} className='card relative bg-white rounted-md shadow-md transition-transform ease-in duration-300 trasnform hover: shadow-lg hover:-translate-y-2'>
             <img src={src} className='h-56 w-full object-cover rounded-t-md' alt=''/>
             <div className="card-body p-4">
                 <h5 className="text-x1 font-semibold mb-2text-gray-600">{nome}</h5>
