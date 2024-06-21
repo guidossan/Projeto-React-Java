69a22242973ef6bb3caebf1047b3eff74e987ab9
Guilherme Martins <guimsl2004@gmail.com>
2024-06-21 16:14:25 -0300

    pegando imagem

--- imagelite/src/components/index.ts
+++ imagelite/src/components/index.ts
@@ -1,5 +1,6 @@
 import { InputText } from './input/InputText';
-export { Template } from './Template';
+export { Template, RenderIf } from './Template';
 export { ImageCard } from './ImageCard';
 export { Button } from './button';
 export { InputText } from './input';
+
