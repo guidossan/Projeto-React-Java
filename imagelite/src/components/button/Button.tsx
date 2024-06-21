cbea1d2fdcc3ef14c079ac75402633cf1b27a7dc
Guilherme Martins <guimsl2004@gmail.com>
2024-06-21 15:29:40 -0300

    botaoes de submit e voltar

--- imagelite/src/components/button/Button.tsx
+++ imagelite/src/components/button/Button.tsx
@@ -4,13 +4,15 @@ interface ButtonProps {
     style?: string;
     label?: string;
     onClick?: (event: any) => void;
+    type?: "submit" | "button" | undefined
 
 }
 export const Button: React.FC<ButtonProps> = ({
-        onClick, style, label
+        onClick, style, label, type
 }) => {
     return (
         <button className={`${style} text-white px-4 py-2 rounded-lg `} onClick={onClick}>
+            type={type}
             { label }
         </button>
     )
