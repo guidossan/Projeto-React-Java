24dc8dd190715aeee95e8e41479d5defbc8fcdfb
Guilherme Martins <guimsl2004@gmail.com>
2024-06-25 15:21:56 -0300

    entidade user e accesstoken

---
+++ demo/src/main/java/com/guilherme/demo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.guilherme.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.guilherme.demo.domain.entity.User;
+
+
+public interface UserRepository extends JpaRepository<User, String>{
+
+    
+}
