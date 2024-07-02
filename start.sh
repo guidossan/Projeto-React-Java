e345d29eee94fb8d965be78e1011e7a85951e6c3
Guilherme Martins <guimsl2004@gmail.com>
2024-07-02 17:57:08 -0300

    docker compose

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ docker-compose down
 
 docker build -t back-end:latest ./demo
 
-docker build -t back-end:latest ./imagelite
+docker build -t front-end:latest ./imagelite
 
 docker-compose up --build --force-recreate --remove-orphans
